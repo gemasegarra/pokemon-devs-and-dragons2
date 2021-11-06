331956e930acdef95091ec3004e5a4a6f347669d
josepebel <josepe.beldad@gmail.com>
2021-11-06 14:07:37 +0100

    added backend projects and bootstrap

---
+++ backend-app/trainers-service/src/main/java/com/ironhack/trainersservice/TrainersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.trainersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainersServiceApplication.class, args);
+	}
+
+}
---
+++ backend-app/trainers-service/src/test/java/com/ironhack/trainersservice/TrainersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.trainersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
