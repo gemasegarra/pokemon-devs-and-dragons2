e0d5cb9a71057194feb7b7a192a73b58925b0b9a
Sandra <sandra.delparc@gmail.com>
2021-11-16 08:12:22 +0100

    Teams Client added

--- backend-app/edge-service/src/main/java/com/ironhack/edgeservice/controller/impl/EdgeControllerImpl.java
+++ backend-app/edge-service/src/main/java/com/ironhack/edgeservice/controller/impl/EdgeControllerImpl.java
@@ -1,9 +1,13 @@
 package com.ironhack.edgeservice.controller.impl;
 
+import com.ironhack.edgeservice.controller.dto.PokemonDTO;
 import com.ironhack.edgeservice.controller.interfaces.EdgeController;
 import com.ironhack.edgeservice.service.interfaces.EdgeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EdgeControllerImpl implements EdgeController {
@@ -13,4 +17,21 @@ public class EdgeControllerImpl implements EdgeController {
     private EdgeService edgeService;
 
 
+    @GetMapping("/pokemons/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<PokemonDTO> getAll(@PathVariable(name = "name") String name) {
+        return edgeService.getAll(name);
+    }
+
+    @PostMapping("/pokemons/{name}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void store(@PathVariable(name = "name") String name, @RequestBody PokemonDTO pokemonDTO) {
+        edgeService.store(name, pokemonDTO);
+    }
+
+    @DeleteMapping("/pokemons/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable(name = "id") int id) {
+        edgeService.delete(id);
+    }
 }
--- backend-app/edge-service/src/main/java/com/ironhack/edgeservice/service/impl/EdgeServiceImpl.java
+++ backend-app/edge-service/src/main/java/com/ironhack/edgeservice/service/impl/EdgeServiceImpl.java
@@ -2,9 +2,12 @@ package com.ironhack.edgeservice.service.impl;
 
 import com.ironhack.edgeservice.client.TeamClient;
 import com.ironhack.edgeservice.client.TrainersClient;
+import com.ironhack.edgeservice.controller.dto.PokemonDTO;
 import com.ironhack.edgeservice.service.interfaces.EdgeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EdgeServiceImpl implements EdgeService {
@@ -16,4 +19,15 @@ public class EdgeServiceImpl implements EdgeService {
     private TrainersClient trainersClient;
 
 
+    public List<PokemonDTO> getAll(String name) {
+        return teamClient.getAll(name);
+    }
+
+    public void store(String name, PokemonDTO pokemonDTO) {
+        teamClient.store(name, pokemonDTO);
+    }
+
+    public void delete(int id) {
+        teamClient.delete(id);
+    }
 }
